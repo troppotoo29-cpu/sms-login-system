ba62e9aea78b22729cef5624f58a8daf6adfb9a4
troppotoo29-cpu <troppotoo29@gmail.com>
2026-07-12 12:29:05 +0300

    Update app.js

--- app.js
+++ app.js
@@ -5,7 +5,7 @@
 
 // CHANGE THIS ONLY IF YOUR WEB APP URL CHANGES
 const API_URL =
-"https://script.google.com/macros/s/AKfycbxy6_hoQeOVMymCfJCcQTQ3MJpSRmzqSooLRDXKCXd3WiNWxtAkUayDKOf_Vhr09Jbk/exec";
+"https://script.google.com/macros/s/AKfycbzanI9vPbSE3Mo9XoeZxmccKvITFZE76CQXSu_HDZsz0KMoKSSizG4MO9plUCWX7mdV/exec";
 
 document.addEventListener("DOMContentLoaded", function () {
 
