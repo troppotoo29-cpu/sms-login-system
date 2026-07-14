c0e3c0394c3940aa66d2e4439685bf69fa197b49
troppotoo29-cpu <troppotoo29@gmail.com>
2026-07-14 16:09:32 +0300

    Update app.js

--- app.js
+++ app.js
@@ -5,7 +5,7 @@
 
 // CHANGE THIS ONLY IF YOUR WEB APP URL CHANGES
 const API_URL =
-"https://script.google.com/macros/s/AKfycbx-9oO77ReV5YbeXHwVRY9L2bvXIEOcXZQ2K5argkvrkiod-wVVpttFg_bOG058Y9pO/exec";
+"https://script.google.com/macros/s/AKfycbxbCSEDNHy3zENVcejysZPXKaG8z1-j1O22aJwmtJ3m7zsdr6EZ2WswjlqBtf47qDFN/execc";
 document.addEventListener("DOMContentLoaded", function () {
 
     const loginButton =
