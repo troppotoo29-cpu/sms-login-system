8125c8ccceb015c5a9f8a0ab53d14ffb11dcf97b
troppotoo29-cpu <troppotoo29@gmail.com>
2026-07-20 12:50:34 +0300

    Update products.js

--- products.js
+++ products.js
@@ -37,6 +37,7 @@ document.addEventListener("DOMContentLoaded", function () {
 
 });
 
+
 function saveProduct(){
 
 const product={
@@ -122,3 +123,121 @@ document.addEventListener("DOMContentLoaded", function () {
     document.getElementById("productsBtn").onclick = openAddProduct;
 
 });
+//======================================
+// ADD PRODUCT
+//======================================
+
+function showAddProduct() {
+
+    document.getElementById("productContent").innerHTML = `
+
+        <label>Product Name</label>
+        <input id="pProduct">
+
+        <label>Category</label>
+        <input id="pCategory">
+
+        <label>Supplier</label>
+        <input id="pSupplier">
+
+        <label>Buying Price</label>
+        <input id="pBuying" type="number">
+
+        <label>Selling Price</label>
+        <input id="pSelling" type="number">
+
+        <label>Opening Stock</label>
+        <input id="pStock" type="number">
+
+        <label>Reorder Level</label>
+        <input id="pReorder" type="number">
+
+        <br><br>
+
+        <button id="saveProductBtn">
+
+            💾 SAVE PRODUCT
+
+        </button>
+
+        <div id="productMessage"></div>
+
+    `;
+
+    document.getElementById("saveProductBtn").onclick = saveProduct;
+
+}
+//======================================
+// IMPORT PRODUCTS
+//======================================
+
+function showImportProducts(){
+
+    document.getElementById("productContent").innerHTML=`
+
+        <h3>📥 Import Products</h3>
+
+        <input type="file" id="excelFile">
+
+        <br><br>
+
+        <button id="previewExcel">
+
+            Preview Excel
+
+        </button>
+
+        <br><br>
+
+        <button id="importProductsBtn">
+
+            IMPORT PRODUCTS
+
+        </button>
+
+        <div id="previewArea"></div>
+
+    `;
+
+}
+//======================================
+// BARCODE LABELS
+//======================================
+
+function showBarcodeLabels(){
+
+    document.getElementById("productContent").innerHTML=`
+
+        <h3>🏷 Barcode Label Center</h3>
+
+        <input id="labelSearch" placeholder="Search Product">
+
+        <br><br>
+
+        <select id="labelLayout">
+
+            <option>21 Labels / A4</option>
+
+            <option>30 Labels / A4</option>
+
+            <option>40 Labels / A4</option>
+
+        </select>
+
+        <br><br>
+
+        <button>
+
+            🖨 PRINT LABELS
+
+        </button>
+
+        <button>
+
+            💾 SAVE PDF
+
+        </button>
+
+    `;
+
+}
